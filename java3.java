8b59b7b8feb3fffd8182e8d390bd5e01a02474da
hodiJavaQa <99143515+hodiJavaQa@users.noreply.github.com>
2022-02-13 21:43:18 +0200

    Create java3.java

---
+++ java3.java
@@ -0,0 +1,91 @@
+ public static void main(String[] args) {
+
+        //System.out.print("hothaifa ");// print with a line after printing
+        //System.out.print("is a good friend ");
+        //System.out.print("hiiii ");
+        //System.out.println(" GOD is Good");
+        //System.out.print("hothaifa");
+        //System.out.print("zoubi");
+        //System.out.print("hothaifa");
+        //System.out.print("hothaifa");
+        /*System.out.print("hothaifa");
+        //System.out.print("hothaifa");
+        //System.out.print("hothaifa");
+        //System.out.print("hothaifa");
+        //System.out.print("hothaifa");
+        //System.out.print("hothaifa");*/
+        
+       
+        //כתבו קוד שמגדיר מספר שלם ובודק אם המספר
+        // חיובי וגם זוגי אז מדפיס אותו
+
+        //int r= 9;
+//
+        //if (r>0 && r%2==0)
+        //    System.out.println(r);
+//
+//
+        //if(r>50 || r%6==0)
+        //    System.out.println("in the OR if");
+        //else
+        //    System.out.println("think again");
+
+        Scanner scanner = new Scanner(System.in);
+        //System.out.println("please enter your name :");
+        ////int age = scanner.nextInt();
+        //String name= scanner.next();
+        //System.out.println("hii "+name);
+
+
+        //תכתבו תוכנית שקולטת מספר מהמשתמש ומדפיסה
+        //מספר /2
+//
+        //int number;
+        //System.out.print("please enter a number :");
+        //number=scanner.nextInt(); // you will get the value from the user
+        //System.out.println(number/2);
+
+        //String food="pizza";
+        //System.out.println(food.charAt(4));
+
+        //int number1,number2;
+        //char op;
+//
+        //System.out.print("please enter the first number :");
+        //number1=scanner.nextInt();
+//
+        //System.out.print("please enter operator :");
+        //op = scanner.next().charAt(0);
+        //System.out.print("please enter the second number :");
+        //number2= scanner.nextInt();
+        //int result;
+        //if(op == '+'){
+        //    result=number1+number2;
+        //    System.out.println(result);
+        //}
+        //else if (op == '-'){
+        //    result=number1-number2;
+        //    System.out.println(result);
+        //}
+//
+
+        System.out.println("please enter your name ");
+        String name1=scanner.next();
+        //System.out.println("please enter tou soulmate name");
+        //String name2 =scanner.next();
+//
+        //if(name1.equals(name2))
+        //    System.out.println("100% in love");
+        //else
+        //    System.out.println("he's not the one ");
+        //if (name1.equals("lian"))
+
+        System.out.println(name1.charAt(4));
+        System.out.println(name1.toUpperCase());
+        System.out.println(name1.toLowerCase());
+        System.out.println(name1.trim());
+        System.out.println(name1.length());
+        System.out.println(name1.replace('a','6'));
+
+
+    }
