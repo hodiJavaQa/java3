5e17014e828ef6faf1e7dd61a73355934f247513
hodiJavaQa <99143515+hodiJavaQa@users.noreply.github.com>
2022-02-13 21:40:52 +0200

    Create homeworksolution.java

---
+++ homeworksolution.java
@@ -0,0 +1,34 @@
+ public static void main(String[] args) {
+
+       
+        // 1
+        int x1=5;
+        int x2=87;
+        
+        if(x1 > x2)
+           System.out.println(x1);
+        else
+            System.out.println(x2);
+        
+
+        //2
+        int y = 1350;
+        if(y%5==0)
+            System.out.println("this is a magic number");
+        else
+            System.out.println("not a magic number ");
+            
+            
+        //3
+       int zone= 8;
+
+       if(zone >= 10  && zone <= 35){
+           System.out.println("in the zone ");
+        }
+        
+        
+       //4
+       
+
+
+}
